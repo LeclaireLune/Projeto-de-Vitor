c9b96c21cd6c55b8532c1e785b1b8a7709c75547
LuisEd83 <eduluis784@gmail.com>
2024-10-14 22:36:34 -0300

    Update main.c

--- main.c
+++ main.c
@@ -60,45 +60,27 @@ void pegarCarta(Carta *deckAlvo, int *numCartasAgora, Carta **baralho, int *tama
     }
     *baralho = newBaralho; // Atualiza o ponteiro do baralho
 }
-int somaCartas(Carta *deckAlvo, int *numCartasAgora){
-    const int soma=0;
-    int *vetorSoma=malloc((*numCartasAgora) * sizeof(int)),valor_1;
-
-    for(int i=0; i<*numCartasAgora; i++){
-        for(int j=1; j=10; j++){
-            if(deckAlvo[i].rank=='J' || deckAlvo[i].rank == 'Q' || deckAlvo[i].rank =='K'){
-                vetorSoma[i]=10;
+int somaCartas(Carta *deckAlvo, int *numCartasAgora) {
+    int soma = 0;
+    int valor;
+    for (int i = 0; i < (*numCartasAgora); i++) {
+        if (strcmp(deckAlvo[i].rank, "A") == 0) {
+            if(soma<=10){
+                soma+=11;
             }else{
-                if(deckAlvo[i].rank == '1'){
-                    do{
-                    printf("Voçê deseja utilizar esta carta como 1 ou como 11? (1/11)\n");
-                        scanf(" %d", &valor_1);
-                        if(valor_1==1){
-                            vetorSoma[i]=1;
-                        }else{
-                            if(valor_1==11){
-                                vetorSoma[i]=11;
-                            }
-                        }
-                    }while(valor_1!=1 || valor_1!=11);
-                }else{
-                    if(deckAlvo[i].rank == 'j'){
-                        vetorSoma[i]=j;
-                    }
-                }
+                soma+=1;
             }
-        }
-        for(int k=0; k<*numCartasAgora; k++){
-            soma=soma+vetorSoma[k];
+        } else if (strcmp(deckAlvo[i].rank, "J") == 0 || strcmp(deckAlvo[i].rank, "Q") == 0 || strcmp(deckAlvo[i].rank, "K") == 0) {
+            soma += 10;
+        } else {
+            soma += atoi(deckAlvo[i].rank); // Converte a string para número
         }
     }
-    free(vetorSoma);
     return soma;
 }
 
 
 int jogoPlayer(Carta *baralho) {
-    //somaCartas(deckAlvo,numCartasAgora);
     int iniCartasPlayer = 2; // N�mero inicial de cartas para o jogador
     int totCartasPlayer = 0; // O jogador comeca com 0
 
@@ -119,7 +101,7 @@ int jogoPlayer(Carta *baralho) {
 
     printf("\n\t\t\tCartas do Jogador:\n");
     for (int i = 0; i < iniCartasPlayer; i++) {
-        printf("\n\t\t\tNaipe: %s // Rank: %s", cartasPlayer[i].naipe, cartasPlayer[i].rank);
+        printf("\n\t\t\tNaipe: %s // Rank: %s\n", cartasPlayer[i].naipe, cartasPlayer[i].rank);
     }
     // La�o de repeti��o para adicionar cartas a mao do Jogador
     do {
@@ -131,12 +113,12 @@ int jogoPlayer(Carta *baralho) {
             if (totCartasPlayer < maxCartasPegas) {
                 pegarCarta(cartasPlayer, &totCartasPlayer, &baralho, &varTamBaralho);
                 printf("\t\t\tNaipe: %s // Rank: %s", cartasPlayer[totCartasPlayer - 1].naipe, cartasPlayer[totCartasPlayer - 1].rank);
-                if (soma > 21){
+                if (somaCartas(cartasPlayer,&totCartasPlayer) > 21){
                     printf("\n\t\t\tVocê perdeu!\n");
                     return -1;
                 }
                 else{
-                    if (soma == 21){
+                    if (somaCartas(cartasPlayer,&totCartasPlayer) == 21){
                         printf("\n\t\t\tVocê Venceu!\n");
                         return 1;
                     }
@@ -145,11 +127,11 @@ int jogoPlayer(Carta *baralho) {
             else {
                 printf ("\t\t\tJogador atingiu o maximo de cartas pegas por turno.");
                 printf ("\n\t\t\tAgora e a vez do Dealer...\n\n");
-                return soma;
+                return somaCartas(cartasPlayer,&totCartasPlayer);
             }
         } else if (opcao2 == 'n') {
             printf ("\n\t\t\tAgora e a vez do Dealer...\n\n");
-            return soma;
+            return somaCartas(cartasPlayer,&totCartasPlayer);
         } else {
             printf("Input Inv�lido.\n");
         }
@@ -162,10 +144,9 @@ int jogoPlayer(Carta *baralho) {
 }
 
 void Dealer(Carta *baralho, int somaPlayer){
-    int Y=1;
     int iniCartasDealer = 1; // Número inicial de cartas para o computador/dealer
     int totCartasDealer = 0; // O dealer comeca com 0
-
+    int totalDealer = 0;
     Carta *cartasDealer = malloc(maxCartasPegas * sizeof(Carta));
     if (cartasDealer == NULL) {
         printf("Ocorreu um erro na memoria...");
@@ -174,29 +155,35 @@ void Dealer(Carta *baralho, int somaPlayer){
 
     // O dealer pega carta uma vez
     pegarCarta(cartasDealer, &totCartasDealer, &baralho, &varTamBaralho);
+    totalDealer = somaCartas(cartasDealer, &totCartasDealer);
+    printf("\n\t\t\tNaipe: %s // Rank: %s\n",cartasDealer[totCartasDealer-1].naipe,cartasDealer[totCartasDealer-1].rank);
 
-    do{
-        if(soma(cartasDealer,totCartasDealer)<21 && soma(cartasDealer,totCartasDealer)<=somaPlayer){
-            pegarCarta(cartasDealer, &totCartasDealer, &baralho, &varTamBaralho);
-            printf("\t\t\tNaipe: %s // Rank: %s", cartasDealer[totCartasDealer - 1].naipe, cartasDealer[totCartasDealer - 1].rank);
-            if(soma(cartasDealer,totCartasDealer)==21 || soma(cartasDealer,totCartasDealer)>somaPlayer){
-                printf("\n\t\t\tVocê perdeu!\n");
-                break;
+    while(totalDealer<17 && totalDealer<somaPlayer){
+        pegarCarta(cartasDealer, &totCartasDealer, &baralho, &varTamBaralho);
+        totalDealer = somaCartas(cartasDealer, &totCartasDealer);
+        printf("\n\t\t\tNaipe: %s // Rank: %s\n",cartasDealer[totCartasDealer-1].naipe,cartasDealer[totCartasDealer-1].rank);
+    }
+
+    if(totalDealer>21){
+        printf("\n\t\t\tVocê venceu! O Dealer estourou com: %d pontos\n",totalDealer);
+    }else{
+        if(totalDealer>somaPlayer){
+            printf("\n\t\t\tVocê perdeu! O Dealer conseguiu: %d pontos\n",totalDealer);
+        }else{
+            if(totalDealer==somaPlayer){
+                printf("\n\t\t\tVocê empatou! Ambos ficaram com: %d pontos\n",totalDealer);
             }else{
-                if(soma(cartasDealer,totCartasDealer)>21){
-                    printf("\n\t\t\tVocê venceu!\n");
-                    break;
-                }
+                printf("\n\t\t\tVocê perdeu! O Dealer ficou com: %d pontos\n",totalDealer);
             }
         }
-        Y++;
-    }while(1);
+    }
+
 
     free(cartasDealer);
 }
 
 int menu() { // Fun��o de menu
-    int x;
+    int somaPlayer;
 
     printf("\t\tBem-vindo ao menu, deseja jogar? y/n ");
     scanf(" %c", &opcao1);
@@ -210,9 +197,10 @@ int menu() { // Fun��o de menu
         geradorBaralho(baralho);
         embaralharBaralho(baralho);
 
-        x=jogoPlayer(baralho);
-        if(x!=1 || x!=-1){
-            jogoDealer(baralho,x);
+        somaPlayer=jogoPlayer(baralho);
+        if(somaPlayer!=1 && somaPlayer!=-1){
+            Dealer(baralho,somaPlayer);
+            return 1;
         }
         free(baralho);
     } else if (opcao1 == 'n') {
